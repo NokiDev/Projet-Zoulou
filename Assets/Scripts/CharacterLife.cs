f358a46075fc86adcb7167fd0ef2ec280408cbfb
etiennegithub <etienne.gobion@ynov.com>
2015-12-02 11:02:26 +0100

    blou

---
+++ Assets/Scripts/CharacterLife.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterLife : MonoBehaviour {
+	public int healthMax ;
+	private int currentHealth ;
+	public int damage;
+	// Use this for initialization
+	void Start () {
+		currentHealth=healthMax;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+
+		if (currentHealth <= 0) 
+		{
+			Destroy(gameObject);
+		} 
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "hostile")
+			currentHealth = currentHealth - damage;
+	}
+}
